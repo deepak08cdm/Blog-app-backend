022e79294a0ec32e869b85682d19f6573090502d
Deepak <deepak08.cdm@gmail.com>
2023-01-06 18:23:15 +0530

    route expanded

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
-app.get('/',(req,res)=>{
+app.get('/api/getData',(req,res)=>{
   res.json({
     message:'hoting data to cyclic'
   })
