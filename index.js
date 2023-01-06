312982598c595cc659b14fdf471e91cddd6f1761
Deepak <deepak08.cdm@gmail.com>
2023-01-06 18:21:40 +0530

    get route added

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
-
+app.get('/',(req,res)=>{
+  res.json({
+    message:'hoting data to cyclic'
+  })
+})
 
 app.listen(process.env.PORT ||port, () => {
   console.log(`Server running on port ${port}`)
